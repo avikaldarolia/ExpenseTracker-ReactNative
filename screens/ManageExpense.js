dafd95bc68d71999aab787e531beaed4d93b011e
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-19 14:26:51 +0530

    Navigation setup and styling

---
+++ screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+function ManageExpense() {
+    return (
+        <Text>ManageExpense screen</Text>
+    )
+}
+
+export default ManageExpense
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+function RecentExpense() {
+    return (
+        <Text>RecentExpense screen</Text>
+    )
+}
+
+export default RecentExpense
