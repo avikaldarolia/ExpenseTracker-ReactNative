e0c4368b23fa84f1939c4f157f99d0e7efbf25d2
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-20 12:44:55 +0530

    Adding custom buttons with styles

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import IconButton from "../components/ui/IconButton";
+import Button from "../components/ui/Button";
+import { GlobalStyles } from "../constants/styles";
 
 function ManageExpense({ route, navigation }) {
 
@@ -12,10 +15,53 @@ function ManageExpense({ route, navigation }) {
         })
     }, [navigation, isEditing])
 
+    function deleteExpenseHandler() {
 
+    }
+    function cancelHandler() {
+
+    }
+    function confirmHandler() {
+
+    }
     return (
-        <Text>ManageExpense screen</Text>
+        <View style={styles.container}>
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode={"flat"} onPress={cancelHandler}>Cancel</Button>
+                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'} </Button>
+            </View>
+            {isEditing &&
+                <View style={styles.deleteContainer}>
+                    <IconButton icon="trash" color={GlobalStyles.colors.error500} size={36} onPress={() => { deleteExpenseHandler }}
+                    />
+                </View>
+            }
+        </View>
     )
 }
 
 export default ManageExpense
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8
+    },
+    deleteContainer: {
+        paddingTop: 8,
+        marginTop: 16,
+        borderWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    }
+})
