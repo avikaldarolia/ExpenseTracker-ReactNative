1963a86b51f016a81e16f5c11b1f39a370e13862
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-20 16:34:01 +0530

    Handling the user input

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,28 +1,45 @@
+import { useState } from "react"
 import { StyleSheet, Text, View } from "react-native"
 import Input from "./Input"
 
 function ExpenseForm() {
-
-    function amountChangedHandler() { }
+    const [inputValues, setInputValues] = useState({
+        amount: '',
+        date: '',
+        description: ''
+    })
+    function inputChangedHandler(inputIdentifier, enteredValue) {
+        setInputValues((currentInputValues) => {
+            return {
+                ...currentInputValues,
+                [inputIdentifier]: enteredValue
+            }
+        })
+    }
     return (
         <View style={styles.form}>
             <Text style={styles.title}>Your Expense</Text>
             <View style={styles.inputsRow}>
                 <Input style={styles.rowInput} label={"Amount"} textInputConfig={{
                     keyboardType: 'decimal-pad',
-                    onChangeText: amountChangedHandler
+                    onChangeText: inputChangedHandler.bind(this, 'amount'),
+                    value: inputValues.amount
                 }} />
                 <Input style={styles.rowInput} label={"Date"} textInputConfig={{
                     placeholder: 'YYYY-MM-DD',
                     maxLength: 10,
-                    onChangeText: () => { },
+                    onChangeText: inputChangedHandler.bind(this, 'date'),
+                    value: inputValues.date
                 }} />
             </View>
             <Input label={"Description"} textInputConfig={{
                 multiline: true,
+                onChangeText: inputChangedHandler.bind(this, 'description'),
+                value: inputValues.description
                 // autoCapitalize: 'none'
                 // autoCorrect: false // default is true.
             }}
+
             />
         </View>
     )
